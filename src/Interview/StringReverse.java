3526f40c618d5bebbf74494ab3edb1d141a0d357
Vladyslav Hanoshenko <vladyslavhanoshenko@gmail.com>
2019-03-04 01:51:39 +0200

    update of StringReverse class

--- src/Interview/StringReverse.java
+++ src/Interview/StringReverse.java
@@ -7,11 +7,23 @@ public class StringReverse {
         for (int i = str.length()-1; i >=0; i--) {
             System.out.print(str.charAt(i));
         }
+        System.out.println();
+    }
+
+    public static void stringReverse2(String str){
+        char[] temp = str.toCharArray();
+
+        for (int i = temp.length-1; i >=0 ; i--) {
+            System.out.print(temp[i]);
+
+        }
+        System.out.println();
 
     }
 
     public static void main(String[] args) {
         stringReverse1("Hello world");
+        stringReverse2("teaHello World");
 
     }
 }
